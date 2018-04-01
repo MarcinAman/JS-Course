a0c6624b699a09aa0034d17bbfae38175d08127a
MarcinAman <marcin.aman@gmail.com>
2018-04-01 21:42:12 +0200

    Fixed overlapping text

--- Lab2/scripts.js
+++ Lab2/scripts.js
@@ -3,7 +3,7 @@ data = {
     width:600,
     height:600,
     font_size:15,
-    lower_gap:30
+    lower_gap:30,
 };
 
 data.diagram_items = new Map();
@@ -20,9 +20,18 @@ function clear_canvas(){
 
 function draw_bar(x,y,w,h,text){
     var ctx = document.getElementById("diagram").getContext("2d");
+
     //text starts at x,0
     ctx.fillStyle = "black";
     ctx.font = data.font_size+"px Arial";
+
+    var text_len_in_pixels = ctx.measureText(text).width;
+
+    if(text_len_in_pixels>w){
+        var last_index = Math.floor(text.length*w/text_len_in_pixels);
+        text = text.substr(0,last_index)+".";
+    }
+
     ctx.fillText(text,x,data.height-data.font_size,w);
     ctx.fillStyle = colors[Math.floor(Math.random()*colors.length)];
     ctx.fillRect(x,data.height-y,w,-h);
@@ -30,6 +39,7 @@ function draw_bar(x,y,w,h,text){
 
 function collect_data(){
     clear_canvas();
+
     document.getElementById("textarea").value.split(" ").filter(a=>a!=='').forEach(function (value) {
         var hash_value = 0;
         if(data.diagram_items.has(value)){
@@ -38,7 +48,10 @@ function collect_data(){
         }
         data.diagram_items.set(value,hash_value+1);
     });
-    console.log(data.diagram_items);
+
+    data.font_size = Math.min(data.width/20,data.font_size);
+
+    data.lower_gap = Math.min(data.height/10,data.font_size*2);
     draw_grid();
 }
 
@@ -57,10 +70,13 @@ function draw_grid() {
     var max_repetitions = get_max_repetitions();
     var w = data.width/(1.1*data.diagram_items.size+0.1);
     for (var [key, value] of data.diagram_items.entries()) {
-        draw_bar(x*index, data.lower_gap, w, 0.90*data.width* value / max_repetitions,key);
+        draw_bar(x*index+0.05*x, data.lower_gap, w,
+            0.90*data.height*value / (max_repetitions),key);
+
+        /* we print only within 90% of canvas height for aesthetics */
+
         index++;
     }
-
 }
 
 
